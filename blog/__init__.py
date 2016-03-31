def11ab64e5c03f14005e49399a0f0b40623a2dc
Chris Angelico <rosuav@gmail.com>
2016-03-31 11:35:12 +1100

    Hack Travis config into __init__

--- blog/__init__.py
+++ blog/__init__.py
@@ -3,6 +3,10 @@ import os
 
 app = Flask(__name__)
 config_path = os.environ.get("CONFIG_PATH", "blog.config.DevelopmentConfig")
+if os.environ.get("TRAVIS"):
+    print("** Hacking config_path to TravisConfig **")
+    print("config_path was: "+config_path)
+    config_path = "blog.config.TravisConfig"
 app.config.from_object(config_path)
 
 from . import views
